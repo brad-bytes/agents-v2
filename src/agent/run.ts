e7560235670a1de569649b344cf1f2dd6d27213f
brad-bytes <248548902+brad-bytes@users.noreply.github.com>
2026-03-22 15:00:02 -0230

    initial commit

---
+++ src/agent/run.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { generateText, type ModelMessage } from "ai";
+import { openai } from "@ai-sdk/openai";
+import { SYSTEM_PROMPT } from "./systemPrompt";
+import type { AgentCallbacks } from "../types";
+const MODEL_NAME = "gpt-5-mini";
+
+
+export const runAgent = async (
+    userMessage: string,
+    conversationHistory: ModelMessage[],
+    callbacks: AgentCallbacks
+) => {
+    const {} = await generateText({
+        model: openai(MODEL_NAME),
+        prompt : userMessage,
+        system : SYSTEM_PROMPT,
+    });
+    console.log(text);    
+};
+
+runAgent("What is the capital of France?");
